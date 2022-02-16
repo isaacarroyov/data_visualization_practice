f5112e5ba586153db259dfd18132e7355949c6ea
Isaac Arroyo <isaacarroyov@outlook.com>
2022-02-15 20:17:44 -0600

    [UPDATE] changed arrows and saved the plot with ggsave

--- R/visualizing_temperatures/visualizing_temperatures_01.R
+++ R/visualizing_temperatures/visualizing_temperatures_01.R
@@ -16,33 +16,27 @@ mid = mean(df$Temperature)
 # Set width and height
 width = 1350
 height = 1080
-w = width * 0.6
-h = height * 0.6
+w = width * 0.7
+h = height * 0.7
 
 # Create arrows coordinates
 arrows1 = tibble(
-  x1 = c(37, 40, 14),
-  x2 = c(mid+0.3, 42.43173, 10.25),
-  y1 = c(11.85,5.4, 2),
-  y2 = c(11.55, 7.8, 1.05)
+  x1 = c(37, 15, 40, 14),
+  x2 = c(mid+0.3, 24.8, 42.43173, 10.25),
+  y1 = c(11.75, 11.6, 5.7, 2),
+  y2 = c(11.55, 12.1, 7.8, 1.05)
 )
 arrows2 = tibble(
-  x1 = c(15),
+  x1 = c(20),
   x2 = c(32),
-  y1 = c(9.1),
-  y2 = c(9.2)
+  y1 = c(6.5),
+  y2 = c(8.9)
 )
-arrows3 = tibble(
-  x1 = c(15),
-  x2 = c(32),
-  y1 = c(7.7),
-  y2 = c(7.7)
-)
-
 
 # Set seed
 set.seed(2022)
-# Base plot
+
+# B A S E   P L O T
 (g = ggplot(df, aes(x=Temperature, y=Month, color=Temperature)) +
   labs(x="Temperature (ºC)", y=NULL,
        title = "19 years of temperature data",
@@ -66,63 +60,66 @@ set.seed(2022)
     axis.line = element_line(colour = 'gray70'),
     panel.grid = element_blank(),
     panel.border = element_blank(),
-    plot.margin = margin(0, 0, 0, 0)
+    #plot.margin = margin(1, 1, 1, 1)
   ))
 
-
+# A D D   P O I N T S 
 (g_plot = g + geom_jitter(height = 0.13, size=3, alpha=0.15) +
   geom_segment(aes(y=Month, yend=Month,
                    x=MonthMeanTemp_History, xend=mid),
-               colour='black', size=0.6) +
-  geom_vline(xintercept = mid, color='black', size=0.6, alpha=0.8) +
-  stat_summary(fun=mean, geom = "point", size=5, colour='black'))
+               colour='black', size=1) +
+  geom_vline(xintercept = mid, color='black', size=1, alpha=0.8) +
+  stat_summary(fun=mean, geom = "point", size=6, colour='black'))
 
+# A D D   T E X T
 (g_plot_text = g_plot + annotate(geom = "text",
-                                x=39, y= 12, size=5.25, color = "gray20",
+                                x=39, y= 11.6, size = 0.0085 * h, color = "gray20",
                                 family = 'Georgia', hjust=0.5,
                                 label = glue::glue("Mean temperature:\n{round(mid,1)} ºC")
 ) +
     annotate(geom="text",
-             x=15,y=8.5, size=5.25, color = "gray20",
+             x=15,y=11.35, size = 0.0085 * h, color = "gray20",
              family = 'Georgia', hjust=0.5,
-             label = "The hottest months are\nApril and May"
+             label = "Monthly average"
 ) +
     annotate(geom="text",
-             x=40,y=4.5, size=5.25, color = "gray20",
+             x=15,y=7.5, size = 0.0085 * h, color = "gray20",
+             family = 'Georgia', hjust=0.5,
+             label = glue::glue("The hottest month\n(on average) is April\nwith {round(max(df$MonthMeanTemp_History),1)} ºC")
+) +
+    annotate(geom="text",
+             x=40,y=4.5, size = 0.0085 * h, color = "gray20",
              family = 'Georgia', hjust=0.5,
              label = glue::glue("The highest recorded\ntemperature is {round(max(df$Temperature),1)} ºC\n({as.character(df$Month[which.max(df$Temperature)])}, {as.character(df$Year[which.max(df$Temperature)])})")
 ) +
     annotate(geom="text",
-             x=14,y=3, size=5.25, color = "gray20",
+             x=14,y=3.5, size = 0.0085 * h, color = "gray20",
              family = 'Georgia', hjust=0.5,
              label = glue::glue("The lowest recorded\ntemperature is {round(min(df$Temperature),1)} ºC\n({as.character(df$Month[which.min(df$Temperature)])}, {as.character(df$Year[which.min(df$Temperature)])})")
   )
 )
 
 
-# Add arrows to plot
+
+# A D D   A R R O W S
 (g_final = g_plot_text + geom_curve(
   data = arrows1,
   aes(x=x1,xend=x2, y=y1, yend=y2),
   arrow = arrow(length = unit(10,'points')),
-  size = 0.7,
+  size = 0.9,
   color='gray20',
-  curvature = -0.2
+  curvature = -0.3
 ) +
   geom_curve(
     data = arrows2,
     aes(x=x1,xend=x2, y=y1, yend=y2),
     arrow = arrow(length = unit(10,'points')),
-    size = 0.7,
+    size = 0.9,
     color='gray20',
-    curvature = -0.2
-  ) +
-  geom_curve(
-    data = arrows3,
-    aes(x=x1,xend=x2, y=y1, yend=y2),
-    arrow = arrow(length = unit(10,'points')),
-    size = 0.7,
-    color='gray20',
-    curvature = 0.2
+    curvature = 0.3
   ))
 
+# Save the plot 
+ggsave('visualizing_temperatures_R_01.png',
+       width = w, height = h,
+       units = "px", dpi = 300, scale = 3)
