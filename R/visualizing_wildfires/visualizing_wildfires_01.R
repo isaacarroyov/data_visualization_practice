7e40522781471e564f37d52237ba162f321dac62
Isaac Arroyo <isaacarroyov@outlook.com>
2022-03-05 03:53:48 -0600

    [UPDATE] Final script -> visualizing wildfires with cartograms

--- R/visualizing_wildfires/visualizing_wildfires_01.R
+++ R/visualizing_wildfires/visualizing_wildfires_01.R
@@ -2,11 +2,10 @@ library(dplyr)
 library(sf)
 library(geojsonsf)
 library(ggplot2)
-#library(ggforce)
+library(ggtext)
 library(cartogram)
-#library(cowplot)
-#library(patchwork)
-#library(MetBrewer)
+library(MetBrewer)
+library(scales)
 
 
 # Load data
@@ -36,10 +35,16 @@ carto_PercentageANPHectares <- sf_mex_states %>%
 
 
 # General theme
-theme_set(theme_light(base_family = "Georgia"))
+theme_set(theme_light())
 theme_update(
-    panel.background = element_rect(fill = "#ffffff"),
-    plot.background = element_rect(fill='#ffffff'),
+    plot.title = element_markdown(family = "Palatino", face="bold", margin = margin(0,0,15,0), size = 38, hjust = 0.5),
+    plot.caption = element_markdown(family='Palatino', size=10, color = "gray60"),
+    legend.title = element_markdown(family = "Georgia", face="bold", size = 15),
+    legend.text = element_text(family="Georgia", size = 9, face="bold"),
+    legend.background = element_blank(),
+    panel.background = element_blank(),
+    #plot.background = element_rect(fill='transparent'), # To edit on Vectornator
+    plot.background = element_rect(fill = "#FFF6EE"), # To publish individual maps on social media
     plot.title.position = "plot",
     plot.caption.position = "plot",
     legend.position = "top",
@@ -54,25 +59,53 @@ theme_update(
     
 )
 
+
+
+
 # Create individual plots
 (map_TotalHectares <- carto_TotalHectares %>%
         ggplot(aes(fill=TotalHectares)) +
         geom_sf(color='transparent') +
-        labs(title="Total hectares", caption = "viz by me")
+        labs(title="Affected area",
+             caption = "**Data**: Comisión Nacional Forestal<br>(CONAFOR), 2017<br><br>**Visualization by Isaac Arroyo<br>(@unisaacarroyov)**"
+             ) +
+        scale_fill_gradientn(colours = met.brewer("Greek"), labels = scales::comma, breaks=seq(0,190000,25000)) +
+        guides(fill = guide_colorbar(title = "Hectares", title.position = "top",
+                                     title.hjust = 0.5, barwidth = unit(400,"points"),
+                                     barheight = unit(10,"points"), ticks=F))
 )
+#ggsave('./../map_affected_area_total.png', width = 756, height = 756, units = "px", dpi = 300, scale = 3)
+#ggsave('./../map_affected_area_total_social_media.png', width = 756, height = 756, units = "px", dpi = 300, scale = 3)
 
 
 (map_NumWildfires <- carto_NumWildfires %>%
         ggplot(aes(fill=NumWildfires)) + 
         geom_sf(color='transparent') + 
-        labs(title="Num Wildfires", caption = "viz by me")
+        labs(title="Number of wildfires",
+             caption = "**Data**: Comisión Nacional Forestal<br>(CONAFOR), 2017<br><br>**Visualization by Isaac Arroyo<br>(@unisaacarroyov)**"
+        ) +
+        scale_fill_gradientn(colours = met.brewer("Greek"), labels = scales::comma, breaks=seq(0,1500,150)) +
+        guides(fill = guide_colorbar(title = "Count", title.position = "top",
+                                     title.hjust = 0.5, barwidth = unit(400,"points"),
+                                     barheight = unit(10,"points"), ticks=F))
+        
 )
+#ggsave('./../map_number_of_wildfires.png', width = 756, height = 756, units = "px", dpi = 300, scale = 3)
+#ggsave('./../map_number_of_wildfires_social_media.png', width = 756, height = 756, units = "px", dpi = 300, scale = 3)
 
 
 (map_PercentageANPHectares <- carto_PercentageANPHectares %>%
         ggplot(aes(fill=PercentageANPHectares)) +
         geom_sf(color="transparent") +
-        labs(title="Percentage ANP ha", caption = "viz by me")
+        labs(title=" Affected area that was an NPA",
+             caption = "**Data**: Comisión Nacional Forestal<br>(CONAFOR), 2017<br><br>**Visualization by Isaac Arroyo<br>(@unisaacarroyov)**"
+        ) +
+        scale_fill_gradientn(colours = met.brewer("Greek"), labels = scales::comma, breaks=seq(0,100,10)) +
+        guides(fill = guide_colorbar(title = "Percentage (%)", title.position = "top",
+                                     title.hjust = 0.5, barwidth = unit(400,"points"),
+                                     barheight = unit(10,"points"), ticks=F))
 )
 
 
+#ggsave('./../map_affected_area_anp.png', width = 756, height = 756, units = "px", dpi = 300, scale = 3)
+#ggsave('./../map_affected_area_anp_social_media.png', width = 756, height = 756, units = "px", dpi = 300, scale = 3)
